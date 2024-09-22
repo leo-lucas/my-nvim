3867738fef04d6c539d4c8a8cfe3aa4cfe22340d
Leonardo Lucas dos Santos <leolucassang@gmail.com>
2024-09-22 22:33:22 +0000

    add configs, test formating

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -2,42 +2,43 @@ local function config()
 	require 'plugins.lsp.lua-ls'
 
 	vim.diagnostic.config {
-    		underline        = false,
-    		virtual_text     = false,
-    		signs            = true,
-    		update_in_insert = true,
-    		severity_sort    = true,
+		underline        = false,
+		virtual_text     = false,
+		signs            = true,
+		update_in_insert = true,
+		severity_sort    = true,
 		float            = {
-        		border = 'rounded',
-        		source = 'always',
-        		header = '',
-        		prefix = '',
-    		}
+			border = 'rounded',
+			source = 'always',
+			header = '',
+			prefix = '',
+		}
 	}
 
 	require("mason").setup()
-	require("mason-lspconfig").setup{
-		ensure_installed = { "lua_ls", "eslint", "tsp_server"  },
+	require("mason-lspconfig").setup {
+		ensure_installed = { "lua_ls", "eslint", "tsp_server" },
 	}
-
-	
-
+	vim.opt.tabstop = 2
+	vim.opt.shiftwidth = 2
+	vim.opt.smartindent = true -- Habilita indentação inteligente baseada no código
+	vim.opt.autoindent = true
 end
 
 return {
-    {
-        'neovim/nvim-lspconfig',
-        config = config,
-        opt = { inlay_hints = { enabled = true } },
-    },
-    -- Lua Deps
-    {'williamboman/mason.nvim' },
-    {'williamboman/mason-lspconfig.nvim'},
-    -- js/ts
-    {
-    	"pmizio/typescript-tools.nvim",
-  	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  	opts = {},
-    },
-    { 'jose-elias-alvarez/null-ls.nvim' }
+	{
+		'neovim/nvim-lspconfig',
+		config = config,
+		opt = { inlay_hints = { enabled = true } },
+	},
+	-- Lua Deps
+	{ 'williamboman/mason.nvim' },
+	{ 'williamboman/mason-lspconfig.nvim' },
+	-- js/ts
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+	{ 'jose-elias-alvarez/null-ls.nvim' }
 }
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,1 +1,9 @@
-return { 'nvim-treesitter/nvim-treesitter'}
+function config()
+	require'nvim-treesitter.configs'.setup {
+  -- A list of pahttps://github.com/tree-sitter/tree-sitter-typescriptrser names, or "all" (the listed parsers MUST always be installed)
+  	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "typescript", "javascript" },
+  	}
+
+end
+
+return { 'nvim-treesitter/nvim-treesitter', config = config}
